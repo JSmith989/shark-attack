eafd28270f7353d53da60e4ec06464466d951580
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-11-12 19:43:40 -0600

    students are displaying on dom

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.scss';
+import SharkTank from '../components/SharkTank';
+import studentData from '../helpers/data/studentData';
 
 class App extends React.Component {
+  state = {
+    livingStudents: studentData.livingStudents(),
+    deadStudents: studentData.dearlyBeloved(),
+  };
+
   render() {
+    const { livingStudents } = this.state;
     return (
-      <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+      <div className='App'>
+        <h2>SHARK ATTACK</h2>
+        <SharkTank livingStudents={livingStudents} />
       </div>
     );
   }
---
+++ src/components/LiveStudent/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+  Card,
+  CardTitle,
+  CardImg,
+  CardBody,
+} from 'reactstrap';
+
+class LiveStudent extends Component {
+  render() {
+    const { firstName, lastName, imageUrl } = this.props.student;
+
+    return (
+      <div className='col-3'>
+        <Card>
+          <CardBody>
+            <CardTitle tag='h5'>
+              {firstName} {lastName}
+            </CardTitle>
+          </CardBody>
+          <CardImg top width='100%' src={imageUrl} alt='Student Image' className="rounded-circle" />
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default LiveStudent;
